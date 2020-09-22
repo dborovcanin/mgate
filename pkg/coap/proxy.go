e5da80532d4dc3a94b74795abb359d56c51f6faa
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2020-09-22 18:06:15 +0200

    Add CoAP Proxy

--- pkg/coap/proxy.go
+++ pkg/coap/proxy.go
@@ -1,13 +1,20 @@
 package mqtt
 
 import (
+	"crypto/x509"
 	"fmt"
 	"io"
 	"net"
+	"time"
 
 	"github.com/mainflux/mainflux/logger"
 	"github.com/mainflux/mainflux/pkg/errors"
 	"github.com/mainflux/mproxy/pkg/session"
+)
+
+const (
+	network   = "udp"
+	maxPktLen = 1500
 )
 
 // Proxy is main MQTT proxy struct
@@ -29,29 +36,39 @@ func New(address, target string, handler session.Handler, logger logger.Logger) 
 	}
 }
 
-func (p Proxy) accept(l net.Listener) {
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			p.logger.Warn("Accept error " + err.Error())
-			continue
-		}
+func (p Proxy) accept(l *net.UDPConn) {
+	buff := make([]byte, maxPktLen)
 
-		p.logger.Info("Accepted new client")
-		go p.handle(conn)
+	for {
+		nr, _, err := l.ReadFromUDP(buff)
+		if err != nil {
+			if neterr, ok := err.(net.Error); ok && (neterr.Temporary() || neterr.Timeout()) {
+				time.Sleep(5 * time.Millisecond)
+				continue
+			}
+			// return err
+		}
+		tmp := make([]byte, nr)
+		copy(tmp, buff)
+		p.handle(l)
+		// go handlePacket(listener, tmp, addr, rh)
+
+		// p.logger.Info("Accepted new client")
+		// go p.handle(conn)
 	}
 }
 
 func (p Proxy) handle(inbound net.Conn) {
 	defer p.close(inbound)
-	outbound, err := p.dialer.Dial("tcp", p.target)
+	uaddr, err := net.ResolveUDPAddr(network, p.target)
 	if err != nil {
-		p.logger.Error("Cannot connect to remote broker " + p.target + " due to: " + err.Error())
-		return
+		p.logger.Warn("Unable to resolve target address" + p.target)
 	}
+
+	outbound, err := net.DialUDP(network, nil, uaddr)
 	defer p.close(outbound)
 
-	s := session.New(inbound, outbound, p.handler, p.logger)
+	s := session.New(inbound, outbound, p.handler, p.logger, x509.Certificate{})
 
 	if err = s.Stream(); !errors.Contains(err, io.EOF) {
 		p.logger.Warn("Broken connection for client: " + s.Client.ID + " with error: " + err.Error())
@@ -60,11 +77,15 @@ func (p Proxy) handle(inbound net.Conn) {
 
 // Proxy of the server, this will block.
 func (p Proxy) Proxy() error {
-	l, err := net.Listen("tcp", p.address)
+	uaddr, err := net.ResolveUDPAddr(network, p.address)
 	if err != nil {
 		return err
 	}
-	defer l.Close()
+
+	l, err := net.ListenUDP(network, uaddr)
+	if err != nil {
+		return err
+	}
 
 	// Acceptor loop
 	p.accept(l)
